0f1781c7a4793233322b68274cf82f5c448f30c9
goosLy1 <gray.goosly@gmail.com>
2023-07-12 00:03:16 +0300

    Add delete channel functional

--- models.py
+++ models.py
@@ -9,15 +9,17 @@ class Channel(Base):
 
     id = Column(String, primary_key=True)
     title = Column(String)
-    statistics = relationship("ChannelStatistics", back_populates="channel")
-    videos = relationship("Video", back_populates="channel")
+    statistics = relationship(
+        "ChannelStatistics", back_populates="channel")
+    videos = relationship("Video", back_populates="channel", cascade="all, delete",
+                          passive_deletes=True,)
 
 
 class ChannelStatistics(Base):
     __tablename__ = "channel_statistics"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    channel_id = Column(String, ForeignKey("channels.id"))
+    channel_id = Column(String, ForeignKey("channels.id", ondelete="CASCADE"))
     view_count = Column(Integer)
     subscriber_count = Column(Integer)
     video_count = Column(Integer)
@@ -29,18 +31,20 @@ class Video(Base):
     __tablename__ = "videos"
 
     id = Column(String, primary_key=True)
-    channel_id = Column(String, ForeignKey("channels.id"))
+    channel_id = Column(String, ForeignKey("channels.id", ondelete="CASCADE"))
     title = Column(String)
     pub_date = Column(DateTime)
     channel = relationship("Channel", back_populates="videos")
-    statistics = relationship("VideoStatistics", back_populates="video")
+    statistics = relationship(
+        "VideoStatistics", back_populates="video", cascade="all, delete",
+        passive_deletes=True,)
 
 
 class VideoStatistics(Base):
     __tablename__ = "video_statistics"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    video_id = Column(String, ForeignKey("videos.id"))
+    video_id = Column(String, ForeignKey("videos.id", ondelete="CASCADE"))
     view_count = Column(Integer)
     like_count = Column(Integer)
     comment_count = Column(Integer)
